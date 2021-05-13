d22df75053cfb777238c5c04939d5f86a2eb03f6
vinicius rosa <viniciushrk@gmail.com>
2021-05-13 03:43:46 +0000

    delete projet not using

--- AppEcotecs/src/pages/LogIn/LogIn.tsx
+++ AppEcotecs/src/pages/LogIn/LogIn.tsx
@@ -13,6 +13,7 @@ import {
 } from 'react-native';
 import LogoI from '../../images/logoI.png';
 import Input from '../../components/Input/Index';
+import {styles} from './Styles';
 
 export default function LogIn(){
     return ( 
@@ -55,55 +56,3 @@ export default function LogIn(){
         </SafeAreaView>
     );
 }
-
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'space-around',
-        paddingHorizontal:10,
-        marginBottom:0
-    },
-    image: {
-        height: Dimensions.get('window').width * 0.6,
-        margin:0
-    },
-    card:{
-        alignItems:'center',
-        marginTop:0,
-        // backgroundColor:'#206A5D',
-        height: Dimensions.get('window').width * 0.7,
-        width: 321,
-        borderRadius:13
-    },
-    titulo:{
-        fontSize:25,
-        color:'black',
-        marginBottom:25
-    },
-    button:{
-        width: '90%',
-        height: 60,
-        paddingHorizontal:16,
-        backgroundColor: '#03ff5f',
-        borderRadius: 10,
-        marginTop: 8,
-
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    textButton:{
-        color: '#312e38',
-        fontSize:18
-    },
-    link:{
-        marginTop: 24,
-        fontSize:20
-    },
-    forgotPasswordText:{
-        color: '#000',
-        fontSize: 16,
-        // fontFamily: 'RobotoSlab-Regular';
-    }
-})
